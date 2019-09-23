4d499df676f2cce13b7ba6b93fef659402342ed9
NajamShehzad <najambutt195@gmail.com>
2019-09-23 21:26:23 +0500

    Test Button Added

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ class App extends React.Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
         </p>
+          <button>
+            Test Button
+          </button>
           <a
             className="App-link"
             href="https://reactjs.org"
